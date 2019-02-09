6acc9e60d2e4e05ccd090b6720e39dbc6d608378
Parick Cowan <patrick.w.cowan@gmail.com>
2019-02-09 13:01:42 -0600

    added files

--- js/JavaScript Test.js
+++ js/JavaScript Test.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/////--------------------------------FUNCTION BONUS WORK---------------------------------////
 
 // Write a function, isShortWord, that takes in a string and
 // returns the boolean value true if the passed argument is shorter than 5 characters. Return false otherwise.
@@ -189,7 +190,8 @@ isLowerCase('C'); //False
         return string !== string.toUpperCase()
 
     }
-    console.log(hasLowerCase("Hi")); // True
+
+hasLowerCase("Hi"); // True
 
 // Make a function named isSpace(letter) that returns if a character is a space character
 // Make a function named isZero(number)
@@ -222,5 +224,60 @@ isLowerCase('C'); //False
 // Make a function named absoluteValue(number) that returns the absolute value of a number.
 // Make a function named rollDice(sides) that takes in an argument containing the number of sides the die should have. Generate a random number between 1 up to and including the number of sides.
 
+/////--------------------------------CONDITIONALS BONUS WORK---------------------------------////
+
+// Write a function makeJustinMessage() that returns the string "It's Justin!"
+// if the input to the function is the string "justin" and "It's not Justin" otherwise.
+//     If the input contains white space or numbers, return the string "INVALID INPUT".
+
+function makeJustinMessage() {
+    return "It's Justin!"
+}
+
+console.log(makeJustinMessage());
 
 
+// Write a function that returns a string describing the action to be taken for a given street light color input. For example, if the function input is the string "red", a message like "STOP!" could be returned from the function. Input should not be case sensitive.
+//
+//     __
+// Create a function that simply returns an input as an output. If no input is passed to the function when it is called, return the string "no input provided."
+//
+// __
+// Create a function that returns a unique message string for a given digit input. If the input is not a single digit, return the string "too many digits". If the input is not a number, return "invalid input". String digits like "1" or "9" are valid inputs.
+//
+//     __
+// Create a function that prompts a user for their favorite day of the week and alerts a unique message based on the day.
+//     Catch any invalid inputs (not indicating a day of the week).
+// For each day, allow the user to enter the abbreviated day (e.g. 'Monday', 'monday', 'Mon', or 'mon')
+//
+// __
+// Create a function that prompts the user for an input and then alerts if the input is a number or not a number.
+// - use an if/else
+//     - refactor to use a ternary operator
+//
+// __
+// Create a function that prompts a user for a season (Spring, Summer, Fall (or Autumn), Winter). The function then alerts the months available in that season and then asks the user to pick a given month. After selecting the month, alert a unique message for the month.
+// - account for any invalid user input
+// - case of input should not matter
+// - accept both abbreviations and full names of the months
+//
+// __
+// GOLD STAR BONUS
+//
+// Create a distance-unit-conversion application.
+//     Prompt the user for unit of measurement.
+//     Prompt the user for a value.
+//
+//     Prompt the user for a second unit of measurement to convert to.
+//
+//     Possibly define multiple functions to convert: inchesToFeet, feetToMiles, milesToLightYears
+// then the opposite versions: lightYearsToMiles, milesToFeet, feetToInches
+//
+// Use these conversion functions to make the correct unit conversion
+//
+// Potentially, you will need a large switch case to account for the possible unit conversions,
+//     i.e. what conversion functions will need to be called in and in what order.
+//
+//     DOUBLE GOLD STAR BONUS
+//
+//     Allow unit conversion to metric system units
