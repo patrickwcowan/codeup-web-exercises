48150f1657c4c5276087ae06947e2bd728c354ed
Parick Cowan <patrick.w.cowan@gmail.com>
2019-02-21 08:27:23 -0600

    added updated file

---
+++ js/Volume_Bonus.js
@@ -0,0 +1,16 @@
+// Create an application that will calculate the volume of a building
+// based on user input.Account for multiple rooms and a user choice of measurement
+// (between imperial or metric).
+
+var buildLength = "";
+    height = "";
+
+
+
+function length() {
+   buildLength = prompt("What is the length of your room:");
+
+}
+
+length();
+
